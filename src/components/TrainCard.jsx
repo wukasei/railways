40276bbd06a63aab92711f31b1dbcf0ba4669c76
Inna <bohachuk.inna@gmail.com>
2026-05-09 13:47:07 +0300

    add train data and TrainCard component

---
+++ src/components/TrainCard.jsx
@@ -0,0 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+import styles from './TrainCard.module.css';
+
+const TrainCard = ({ train }) => {
+    const navigator = useNavigate();
+    const formatDate = (dateStr) => {
+        return new Date(dateStr).toLocaleString('uk-UA', {
+            day: '2-digit',
+            month: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+        });
+    };
+
+    const handleBookingNavigate = () => {
+        navigator(`/booking/${train.id}`);
+    }
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.header}>
+                <span className={styles.trainNumber}>№ {train.number}</span>
+                <span className={styles.duration}>{train.duration} в дорозі</span>
+            </div>
+            
+            <div className={styles.route}>
+                <h3>{train.from} → {train.to}</h3>
+            </div>
+            
+            <div className={styles.footer}>
+                <div className={styles.departureInfo}>
+                    <p className={styles.departureTime}>
+                        Відправлення: <br />
+                        <strong>{formatDate(train.departure)}</strong>
+                    </p>
+                    <div style={{ marginBottom: '10px' }}></div> 
+                    <p className={styles.departureTime}>
+                        Прибуття: <br />
+                        <strong>{formatDate(train.arrival)}</strong>
+                    </p>
+                </div>
+                <button className={styles.bookingBtn} onClick={handleBookingNavigate}>
+                    Вибрати місця
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default TrainCard;
